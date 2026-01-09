d71bcf2a22c1ce32ec11e8b8ab61a8d2f0ed2e70
allivan2020 <allivan2020@gmail.com>
2026-01-09 17:02:55 +0200

    1-st part timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,88 @@
-console.log('hi');
+// Імпорти через Vite (модуль)
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// DOM елементи
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('#start-button');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let selectedDate = null;
+let timerId = null;
+startBtn.disabled = true;
+
+flatpickr(input, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const pickedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (pickedDate <= currentDate) {
+      selectedDate = null;
+      startBtn.disabled = true;
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    } else {
+      selectedDate = pickedDate;
+      startBtn.disabled = false;
+    }
+  },
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+startBtn.addEventListener('click', () => {
+  if (!selectedDate) return;
+
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  timerId = setInterval(() => {
+    const currentTime = new Date();
+    const deltaTime = selectedDate - currentTime;
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      input.disabled = false;
+      updateClockface({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      return;
+    }
+
+    const timeComponents = convertMs(deltaTime);
+    updateClockface(timeComponents);
+  }, 1000);
+});
